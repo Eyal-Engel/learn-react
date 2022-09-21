147e419468fa385a00b17fcc0f043de71db2c950
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2022-09-21 13:05:09 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import Card from "./Card";
+import Navbar from "./Navbar";
+
+function App() {
+  const cards = [
+    {
+      headline: "אתרים",
+      addLabel: "הוספת אתר",
+      image: "../Images/picture_logo.png",
+    },
+    {
+      headline: "חניכים",
+      addLabel: "הוספת חניך",
+      image: "../Images/picture_logo.png",
+    },
+    {
+      headline: "מסלולים",
+      addLabel: "הוספת מסלול",
+      image: "../Images/picture_logo.png",
+    },
+    {
+      headline: "מקצועות",
+      addLabel: "הוספת מקצוע",
+      image: "../Images/picture_logo.png",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <div className="left">Dashboards</div>
+        <div className="right">
+          <div className="cardsLine">
+            <Card cards={cards[0]} />
+            <Card cards={cards[1]} />
+          </div>
+          <div className="cardsLine">
+            <Card cards={cards[2]} />
+            <Card cards={cards[3]} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
