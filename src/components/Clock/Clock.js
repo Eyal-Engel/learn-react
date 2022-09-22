94a7deab2296c70557ce9d59e89d15c433185c87
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2022-09-22 11:39:14 +0300

    try3

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-function Clock() {
-    const [clockState, setClockState] = useState();
+// function Clock() {
+//     const [clockState, setClockState] = useState();
 
-    useEffect(() => {
-        setInterval(() => {
-            const date = new Date();
-            setClockState(date.toLocaleTimeString());
-        }, 1000);
-    }, []);
+//     useEffect(() => {
+//         setInterval(() => {
+//             const date = new Date();
+//             setClockState(date.toLocaleTimeString());
+//         }, 1000);
+//     }, []);
 
-    return <div className="clock">{clockState}</div>;
-}
+//     return <div className="clock">{clockState}</div>;
+// }
 
-export default Clock;
+// export default Clock;
